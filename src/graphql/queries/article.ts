9d48e940efc71f146a967094b128b8a7f4689272
DavidFilth <davidfilth.197694@gmail.com>
2017-11-24 14:12:34 -0800

    Added articlesquery to fetch the articles with the id of the company

--- src/graphql/queries/article.ts
+++ src/graphql/queries/article.ts
@@ -14,5 +14,17 @@ export default {
         resolve(root: object, args: {id: string}) {
             return articleModel.findById(args.id).exec();
         }
+    },
+    Articles: {
+        type: new GraphQLList(articleType),
+        args: {
+            company: {
+                name: "id",
+                type: new GraphQLNonNull(GraphQLID),
+            }
+        },
+        resolve(root, args) {
+            return articleModel.find({company: args.company}).exec();
+        }
     }
 };
