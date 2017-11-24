801e2568349451e5ce247d4c6762c54b63f7a980
DavidFilth <davidfilth.197694@gmail.com>
2017-11-24 14:12:14 -0800

    Added activities query to fetch the activities with the id of the company

--- src/graphql/queries/activity.ts
+++ src/graphql/queries/activity.ts
@@ -14,5 +14,17 @@ export default {
         resolve(root: object, args: {id: string}) {
             return ActivityModel.findById(args.id).exec();
         }
+    },
+    Activities: {
+        type: new GraphQLList(activityType),
+        args: {
+            company: {
+                name: "id",
+                type: new GraphQLNonNull(GraphQLID)
+            }
+        },
+        resolve(root, args) {
+            return ActivityModel.find({company: args.company}).exec();
+        }
     }
 };
