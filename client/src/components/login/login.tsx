455aa76ae2f1fa3d0f7091d0b492df494250a61f
DavidFilth <davidfilth.197694@gmail.com>
2017-12-01 15:58:06 -0800

    components working with field validation and InputField Component

--- client/src/components/login/login.tsx
+++ client/src/components/login/login.tsx
@@ -1,28 +1,53 @@
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { ruleRunnner, Rules, run  } from '../../util/validation';
 import { client } from '../../util/graphql/client';
-import * as update from 'immutability-helper';
 import { ApolloQueryResult } from 'apollo-client';
+import * as update from 'immutability-helper';
+import InputField from '../util/inputField';
 import * as React from 'react';
 import gql from 'graphql-tag';
-import './style.css';
 
-export interface LoginProps {
+export interface LoginProps extends RouteComponentProps<{}> {
     onLogUserIn(user: CustomInterfaces.UserInterface): void;
 }
+export interface LoginState {
+    form: {
+        email: string,
+        password: string
+    };
+    isValid: boolean;
+    validationErrors: CustomInterfaces.GenericForm | null;
+}
 
-class Login extends React.Component<
-    RouteComponentProps<{}> & LoginProps , 
-    {form: {email: string, password: string}}> {
-    constructor(props: RouteComponentProps<{}> & LoginProps) {
+const fieldValidation = [
+    ruleRunnner('email', 'Email', Rules.required, Rules.email),
+    ruleRunnner('password', 'Password', Rules.minLength(4), Rules.required)
+];
+
+class Login extends React.Component< LoginProps, LoginState> {
+    constructor(props: LoginProps) {
         super(props);
         this.state = {
             form: {
                 email: '',
                 password: ''
-            }
+            },
+            isValid: true,
+            validationErrors: null
         };
         this.handleFieldChanged = this.handleFieldChanged.bind(this);
         this.handleSubmitClicked = this.handleSubmitClicked.bind(this);
+        this.validateForm = this.validateForm.bind(this);
+    }
+    componentWillMount() {
+        this.validateForm();
+    }
+    validateForm(form: CustomInterfaces.GenericForm = this.state.form) {
+        let validators = run(form, fieldValidation);
+        this.setState({
+            validationErrors: validators,
+            isValid: validators === null
+        });
     }
     handleFieldChanged(field: string) {
         return (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,10 +57,21 @@ class Login extends React.Component<
                 }
             });
             this.setState(newState);
+            this.validateForm(newState.form);
         };
+    }
+    fieldHasError(field: string) {
+        if (this.state.validationErrors) {
+            return this.state.validationErrors[field];
+        }
+        return;
     }
     handleSubmitClicked(e: React.UIEvent<HTMLFormElement>) {
         e.preventDefault();
+        this.validateForm();
+        if (!this.state.isValid) {
+            return false;
+        }
         client.mutate({ mutation: gql`
             mutation{
                 login(
@@ -73,9 +109,14 @@ class Login extends React.Component<
         return false;
     }
     render() {
+        let { email, password } = this.state.form;
         return (
             <div className="container">
-                <form className="form-horizontal" onSubmit={this.handleSubmitClicked}>
+                <form 
+                    className="form-horizontal" 
+                    onSubmit={this.handleSubmitClicked}
+                    noValidate={true}
+                >
                     <div className="row">
                         <div className="col-md-3"/>
                         <div className="col-md-6">
@@ -83,61 +124,35 @@ class Login extends React.Component<
                             <hr/>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="email"> Email: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-at"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="a@b.com" 
-                                        type="email" 
-                                        name="email" 
-                                        id="email"
-                                        onChange={this.handleFieldChanged('email')}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="password"> Password: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-key"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="1234" 
-                                        type="password" 
-                                        id="password" 
-                                        onChange={this.handleFieldChanged('password')} 
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
+                    <InputField 
+                        name="email"
+                        id="email"
+                        placeholder="a@b.com"
+                        label="Email"
+                        onChange={this.handleFieldChanged('email')}
+                        value={email}
+                        type="email"
+                        addon="fa-at"
+                        errortext={this.fieldHasError('email')}
+                    />
+                    <InputField
+                        name="password"
+                        id="password"
+                        placeholder="1234"
+                        label="Password"
+                        onChange={this.handleFieldChanged('password')}
+                        value={password}
+                        type="password"
+                        addon="fa-key"
+                        errortext={this.fieldHasError('password')}
+                    />
                     <div className="row" style={{paddingTop: '1rem'}}>
                         <div className="col-md-3"/>
                         <div className="col-md-6">
                             <button 
                                 type="submit" 
                                 className="btn btn-primary btn-lg btn-block"
+                                disabled={!this.state.isValid}
                             >
                                 <i className="fa fa-sign-in"/> Login
                             </button>
--- client/src/components/signup/signup.tsx
+++ client/src/components/signup/signup.tsx
@@ -1,24 +1,37 @@
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { ruleRunnner, run, Rules } from '../../util/validation/index';
 import { client } from '../../util/graphql/client';
 import { ApolloQueryResult } from 'apollo-client';
 import * as update from 'immutability-helper';
+import InputField from '../util/inputField';
 import * as React from 'react';
 import gql from 'graphql-tag';
-import './style.css';
 
-class SignUp extends React.Component<
-    RouteComponentProps<{}>, 
-    {
-        form: {
-            firstName: string;
-            lastName: string;
-            location: string;
-            email: string;
-            password: string,
-            confirmPass: string
-        }
-    }> {
-    constructor(props: RouteComponentProps<{}>) {
+export interface SignUpProps extends RouteComponentProps<{}> {
+    onUserSignUp(): void;
+}
+export interface SignUpState {
+    form: {
+        firstName: string;
+        lastName: string;
+        location: string;
+        email: string;
+        password: string,
+        confirmPass: string
+    };
+    isValid: boolean;
+    validationErrors: CustomInterfaces.GenericForm | null;
+}
+const fieldValidations = [
+    ruleRunnner('firstName', 'First Name', Rules.required),
+    ruleRunnner('lastName', 'Last Name', Rules.required),
+    ruleRunnner('location', 'Location', Rules.required),
+    ruleRunnner('password', 'Password', Rules.required, Rules.minLength(4)),
+    ruleRunnner('confirmPass', 'Confirm Password', Rules.mustMatch('password', 'Password'))
+];
+
+class SignUp extends React.Component<SignUpProps, SignUpState> {
+    constructor(props: SignUpProps) {
         super(props);
         this.state = {
             form: {
@@ -28,10 +41,40 @@ class SignUp extends React.Component<
                 email: '',
                 password: '',
                 confirmPass: ''
-            }
+            },
+            isValid: true,
+            validationErrors: null
         };
         this.handleFieldChanged = this.handleFieldChanged.bind(this);
         this.handleSubmitClicked = this.handleSubmitClicked.bind(this);
+        this.validateForm = this.validateForm.bind(this);
+    }
+    componentWillMount() {
+        this.validateForm();
+    }
+    validateForm(form: CustomInterfaces.GenericForm = this.state.form) {
+        const validators = run(form, fieldValidations);
+        this.setState({
+            validationErrors: validators,
+            isValid: validators === null
+        });
+    }
+    handleFieldChanged(field: string) {
+        return (e: React.ChangeEvent<HTMLInputElement>) => {
+            let newState = update(this.state, {
+                form: {
+                    [field]: {$set: e.target.value}
+                }
+            });
+            this.setState(newState);
+            this.validateForm(newState.form);
+        };
+    }
+    fieldHasError(field: string) {
+        if (this.state.validationErrors) {
+            return this.state.validationErrors[field];
+        }
+        return;
     }
     handleSubmitClicked(e: React.UIEvent<HTMLFormElement>) {
         e.preventDefault();
@@ -64,17 +107,8 @@ class SignUp extends React.Component<
         });
         return false;
     }
-    handleFieldChanged(field: string) {
-        return (e: React.ChangeEvent<HTMLInputElement>) => {
-            let newState = update(this.state, {
-                form: {
-                    [field]: {$set: e.target.value}
-                }
-            });
-            this.setState(newState);
-        };
-    }
     render() {
+        let { firstName, lastName, location, email, password, confirmPass } = this.state.form;
         return (
             <div className="container">
                 <form 
@@ -89,150 +123,73 @@ class SignUp extends React.Component<
                             <hr/>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="firstname"> First Name: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-address-card"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="Jhonny" 
-                                        type="text" 
-                                        id="firstname"
-                                        onChange={this.handleFieldChanged('firstName')}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="lastname"> Last Name: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-address-card-o"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="Smith" 
-                                        type="text" 
-                                        id="lastname"
-                                        onChange={this.handleFieldChanged('lastName')}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="location"> Location: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-map-marker"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="Anaheim,CA" 
-                                        type="text" 
-                                        id="location"
-                                        onChange={this.handleFieldChanged('location')}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="email"> Email: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-at"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="a@b.com" 
-                                        type="email" 
-                                        id="email"
-                                        onChange={this.handleFieldChanged('email')} 
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="password"> Password: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-key"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="1234" 
-                                        type="password" 
-                                        id="password"
-                                        onChange={this.handleFieldChanged('password')}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-3 field-label-responsive">
-                            <label htmlFor="confirmPass"> Confirm Pass: </label>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                                    <div className="input-group-addon">
-                                        <i className="fa fa-repeat"/>
-                                    </div>
-                                    <input 
-                                        className="form-control" 
-                                        placeholder="1234" 
-                                        type="password" 
-                                        id="confirmPass"
-                                        onChange={this.handleFieldChanged('confirmPass')}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            error messages
-                        </div>
-                    </div>
+                    <InputField 
+                        name="firstName"
+                        id="firstname"
+                        placeholder="Jhonny"
+                        label="First Name"
+                        onChange={this.handleFieldChanged('firstName')}
+                        value={firstName}
+                        type="text"
+                        addon="fa-address-card"
+                        errortext={this.fieldHasError('firstName')}
+                    />
+                    <InputField 
+                        name="lastName"
+                        id="lastname"
+                        placeholder="Smith"
+                        label="Last Name"
+                        onChange={this.handleFieldChanged('lastName')}
+                        value={lastName}
+                        type="text"
+                        addon="fa-address-card-o"
+                        errortext={this.fieldHasError('lastName')}
+                    />
+                    <InputField 
+                        name="location"
+                        id="location"
+                        placeholder="Anaheim, CA"
+                        label="Location"
+                        onChange={this.handleFieldChanged('location')}
+                        value={location}
+                        type="text"
+                        addon="fa-map-marker"
+                        errortext={this.fieldHasError('location')}
+                    />
+                    <InputField 
+                        name="email"
+                        id="email"
+                        placeholder="a@b.com"
+                        label="Email"
+                        onChange={this.handleFieldChanged('email')}
+                        value={email}
+                        type="email"
+                        addon="fa-at"
+                        errortext={this.fieldHasError('email')}
+                    />
+                    <InputField 
+                        name="password"
+                        id="password"
+                        placeholder="1234"
+                        label="Password"
+                        onChange={this.handleFieldChanged('password')}
+                        value={password}
+                        type="password"
+                        addon="fa-key"
+                        errortext={this.fieldHasError('password')}
+                    />
+                    <InputField 
+                        name="confirmPass"
+                        id="confirmPass"
+                        placeholder="1234"
+                        label="Confirm Password"
+                        onChange={this.handleFieldChanged('confirmPass')}
+                        value={confirmPass}
+                        type="password"
+                        addon="fa-repeat"
+                        errortext={this.fieldHasError('confirmPass')}
+                    />
+                    
                     <div className="row">
                         <div className="col-md-3"/>
                         <div className="col-md-6">
